6f5593684b46bfdc2ad1c9c8ed6631c389d3934c
Arpita Roy <arpita.roy.workar@gmail.com>
2023-07-02 19:50:10 +0530

    final changes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // javascript
 const getEl = (element) => document.querySelector(element)
- menuBtn = getEl(".menu-btn")
- navMenu = getEl(".menu-list")
- exitBtn = getEl(".exit-btn");
+    menuBtn = getEl(".menu-btn")
+    navMenu = getEl("nav")
+    exitBtn = getEl(".exit-btn");
 menuBtn.addEventListener("click", ()=> {
-    // navMenu.classList.add("open-nav");
-    navMenu.style.transform = 'translateX(0%)'
+    navMenu.classList.add("open-nav");
+    // navMenu.style.transform = 'translateX(0)'
 })
 exitBtn.addEventListener("click", ()=> {
-    // navMenu.classList.remove("open-nav");
-    navMenu.style.transform = 'translateX(100%)'
+    navMenu.classList.remove("open-nav");
+    // navMenu.style.transform = 'translateX(100%)'
 })
